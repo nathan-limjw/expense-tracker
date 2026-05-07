ab163f31604b4bd98537c2b412011f6f66ce67e1
nathan-limjw <nathanaelljw@gmail.com>
2026-05-07 16:01:04 +0800

    feat:add env var for model specs

--- utils/config.py
+++ utils/config.py
@@ -6,3 +6,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/db/test.db")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
+MODEL_TEMPERATURE = os.getenv("MODEL_TEMPERATURE", 0)
+
+CONFIDENCE_THRESHOLD = os.getenv("CONFIDENCE_THRESHOLD", 0.75)
