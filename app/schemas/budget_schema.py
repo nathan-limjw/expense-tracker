a51ac71afd9b787440ffda75e0500c02034cfb4f
nathan-limjw <nathanaelljw@gmail.com>
2026-05-08 00:15:28 +0800

    fix: change column type of month to stringand enforce validation of YYYY-MM format

--- app/schemas/budget_schema.py
+++ app/schemas/budget_schema.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 
 class BudgetBase(BaseModel):
     category: str
-    month: datetime
+    month: str
     limit: float
     user_id: str
+
+    @field_validator("month")
+    @classmethod
+    def validate_month_format(cls, v):
+        try:
+            datetime.strptime(v, "%Y-%m")
+        except ValueError:
+            raise ValueError("month must be in YYYY-MM format")
+        return v
 
 
 class BudgetCreate(BudgetBase):
@@ -22,5 +31,14 @@ class BudgetResponse(BudgetBase):
 
 class BudgetUpdate(BaseModel):
     category: str
-    month: datetime
+    month: str
     limit: float
+
+    @field_validator("month")
+    @classmethod
+    def validate_month_format(cls, v):
+        try:
+            datetime.strptime(v, "%Y-%m")
+        except ValueError:
+            raise ValueError("month must be in YYYY-MM format")
+        return v
