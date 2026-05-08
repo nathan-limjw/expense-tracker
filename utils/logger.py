6bc461a407effcc3bdace80ebd399af871847ae2
nathan-limjw <nathanaelljw@gmail.com>
2026-05-08 10:41:46 +0800

    refactor: change import of loglevel to env var

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,9 @@
 import logging
 import os
 
-from utils.config import LOG_LEVEL
-
 
 def get_logger(name: str):
-    log_level = os.getenv(LOG_LEVEL, "DEBUG")
+    log_level = os.getenv("LOG_LEVEL", "DEBUG")
 
     logger = logging.getLogger(name)
     logger.setLevel(getattr(logging, log_level.upper()))
