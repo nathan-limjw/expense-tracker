87a4da742cce2c8d39715fc7277a00be3994900e
nathan-limjw <nathanaelljw@gmail.com>
2026-05-12 11:14:10 +0800

    feat: add togglelable args for engine based on prod or dev

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from utils.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    **(
+        {"connect_args": {"check_same_thread": False}}
+        if "sqlite" in settings.DATABASE_URL
+        else {}
+    ),
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
