8f6c88e8387c768a884902a81bc42a23b70aefba
nathan-limjw <nathanaelljw@gmail.com>
2026-05-20 02:08:56 +0800

    test: fix mock return value of report agent to have the key 'final_report'

--- tests/integration/test_report_endpoints.py
+++ tests/integration/test_report_endpoints.py
@@ -8,32 +8,34 @@ class TestGetReport:
         self, client, generate_test_user, mock_report_agent
     ):
         mock_report_agent.invoke.return_value = {
-            "month": "2026-05",
-            "total_spent": 164.60,
-            "monthly_budget": 1000,
-            "days_in_period": 31,
-            "current_day": 18,
-            "categories": [
-                {
-                    "category": "Food",
-                    "spent": 8.0,
-                    "budget": 200.0,
-                    "variance": -192.0,
-                    "variance_pct": 4.0,
+            "final_report": {
+                "month": "2026-05",
+                "total_spent": 164.60,
+                "monthly_budget": 1000,
+                "days_in_period": 31,
+                "current_day": 18,
+                "categories": [
+                    {
+                        "category": "Food",
+                        "spent": 8.0,
+                        "budget": 200.0,
+                        "variance": -192.0,
+                        "variance_pct": 4.0,
+                    },
+                    {
+                        "category": "Transport",
+                        "spent": 81.0,
+                        "budget": None,
+                        "variance": None,
+                        "variance_pct": None,
+                    },
+                ],
+                "summary": "Some financial advice",
+                "charts": {
+                    "pie": "https://s3.amazonaws.com/pie.png",
+                    "bar": "https://s3.amazonaws.com/bar.png",
                 },
-                {
-                    "category": "Transport",
-                    "spent": 81.0,
-                    "budget": None,
-                    "variance": None,
-                    "variance_pct": None,
-                },
-            ],
-            "summary": "Some financial advice",
-            "charts": {
-                "pie": "https://s3.amazonaws.com/pie.png",
-                "bar": "https://s3.amazonaws.com/bar.png",
-            },
+            }
         }
 
         payload = ReportCreate(user_id=generate_test_user["id"], month="2026-05")
