87beb761393cbf63398b27f43c8ec55cacf20034
nathan-limjw <nathanaelljw@gmail.com>
2026-05-19 22:21:24 +0800

    test: add integration test for report endpoints

--- tests/integration/test_report_endpoints.py
+++ tests/integration/test_report_endpoints.py
@@ -1,3 +1,5 @@
+import uuid
+
 from app.schemas.report_schema import ReportCreate
 
 
@@ -38,3 +40,49 @@ class TestGetReport:
         response = client.post("/report/", json=payload.model_dump())
 
         assert response.status_code == 200
+
+    def test_user_not_found(self, client):
+        nonexistent_user_id = str(uuid.uuid4())
+        payload = ReportCreate(user_id=nonexistent_user_id, month="2026-05")
+
+        response = client.post("/report/", json=payload.model_dump())
+
+        assert response.status_code == 404
+
+    def test_graph_value_error(self, client, generate_test_user, mock_report_agent):
+        mock_report_agent.invoke.side_effect = ValueError("Invalid input!")
+
+        payload = ReportCreate(user_id=generate_test_user["id"], month="2026-05")
+        response = client.post("/report/", json=payload.model_dump())
+
+        assert response.status_code == 400
+        assert response.json()["detail"] == "Invalid input!"
+
+    def test_runtime_value_error(self, client, generate_test_user, mock_report_agent):
+        mock_report_agent.invoke.side_effect = RuntimeError("Runtime error occurred!")
+
+        payload = ReportCreate(user_id=generate_test_user["id"], month="2026-05")
+        response = client.post("/report/", json=payload.model_dump())
+
+        assert response.status_code == 500
+        assert response.json()["detail"] == "Runtime error occurred!"
+
+    def test_unexpected_value_error(
+        self, client, generate_test_user, mock_report_agent
+    ):
+        mock_report_agent.invoke.side_effect = Exception()
+
+        payload = ReportCreate(user_id=generate_test_user["id"], month="2026-05")
+        response = client.post("/report/", json=payload.model_dump())
+
+        assert response.status_code == 500
+        assert response.json()["detail"] == "Failed to generate report"
+
+    def test_post_with_broken_database(self, broken_read_db_client):
+        nonexistent_user_id = str(uuid.uuid4())
+        payload = ReportCreate(user_id=nonexistent_user_id, month="2026-05")
+
+        response = broken_read_db_client.post("/report/", json=payload.model_dump())
+
+        assert response.status_code == 500
+        assert response.json()["detail"] == "Database error occurred!"
